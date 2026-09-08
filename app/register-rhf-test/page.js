0dffa09ba5ea407a0901b8c8c544abbee67a060f
gthawani-del <gthawani@gmail.com>
2026-09-08 20:51:30 +0530

    Probe attendee options module in production build

--- app/register-rhf-test/page.js
+++ app/register-rhf-test/page.js
@@ -2,7 +2,8 @@
 import {useForm} from 'react-hook-form';
 import {zodResolver} from '@hookform/resolvers/zod';
 import {attendeeDefaultValues,attendeeSchema} from '../../lib/forms/attendee-schema';
+import {countries,roles} from '../../lib/forms/attendee-options';
 export default function RegistrationFormProbe(){
   const {register}=useForm({resolver:zodResolver(attendeeSchema),defaultValues:attendeeDefaultValues});
-  return <main style={{padding:24}}><form><label>Build probe<input {...register('full_name')}/></label></form></main>;
+  return <main style={{padding:24}}><form><label>Build probe<input {...register('full_name')}/></label><select {...register('country')}>{countries.map(country=><option key={country}>{country}</option>)}</select><p>{roles.join(', ')}</p></form></main>;
 }
